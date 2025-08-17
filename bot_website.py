73a8549702eda25f54d0366c7801c8e7d003aaec
danger738 <dangertg738@gmail.com>
2025-08-17 21:59:57 +0530

    Update bot_website.py

--- bot_website.py
+++ bot_website.py
@@ -9,7 +9,7 @@ def home():
     return render_template('index.html')
 
 def run_website():
-    app.run(host='0.0.0.0', port=int(os.getenv('WEBSITE_PORT', '8080'))
+    app.run(host='0.0.0.0', port=int(os.getenv('WEBSITE_PORT', '8080')))
 
 def start_website():
     website_thread = threading.Thread(target=run_website)
